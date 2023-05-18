b783573adf7ba630753f1cb68ea5c0c19e1da322
Ammar Zahid <126045721+Ammarzahid000@users.noreply.github.com>
2023-05-18 12:39:11 +0500

    Add files via upload

---
+++ App2.js
@@ -0,0 +1,22 @@
+function backgroundColor(e) {
+    document.body.style.backgroundColor = e.value;
+}
+
+function textColor(e) {
+    document.body.style.color = e.value;
+}
+
+
+
+// const generateOTP = () =>{
+//     const length = 4;
+//     let otp = "";
+
+//     for(let i = 0; i < length; i++){
+//         otp +=Math.floor(Math.random() * 10);
+//     }
+
+//     return otp;
+// }
+
+// console.log(generateOTP());
